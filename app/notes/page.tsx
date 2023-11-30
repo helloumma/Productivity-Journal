513515c25de67c20ae36aafb1fb4be73b3511e37
ummagohil <umgo0510@gmail.com>
2023-11-30 17:37:22 +0000

    users can see personal view of saved data

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,15 +5,27 @@ import { cookies } from "next/headers";
 export default async function Page() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: notes } = await supabase.from("todo").select();
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  console.log(user?.id);
+
+  if (!user) {
+    // Redirect or handle the case where the user is not authenticated
+    return <div>You need to be authenticated to view this page.</div>;
+  }
+
+  // Fetch todos associated with the authenticated user
+  const { data: todos } = await supabase
+    .from("todo")
+    .select()
+    .eq("userId", user?.id);
+
   return (
     <>
-      {/* @ts-expect-error */}
+      {/*@ts-expect-error */}
       <NewToDo />
-      <pre>{JSON.stringify(notes, null, 2)}</pre>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
     </>
   );
 }
--- components/NewToDo.tsx
+++ components/NewToDo.tsx
@@ -5,9 +5,15 @@ import { cookies } from "next/headers";
 export default async function NewToDo() {
   const addTodo = async (formData: FormData) => {
     "use server";
+
     const title = formData.get("title");
     const supabase = createServerActionClient({ cookies });
-    await supabase.from("todo").insert({ title }).select();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    await supabase.from("todo").insert({ title, userId: user?.id }).select();
+
     revalidatePath("/notes");
   };
 
