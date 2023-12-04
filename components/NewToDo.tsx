ef7f776ba2a28575380557e1dc28e12a84b10c3b
ummagohil <umgo0510@gmail.com>
2023-12-04 20:55:27 +0000

    working version

--- components/NewToDo.tsx
+++ components/NewToDo.tsx
@@ -9,19 +9,14 @@ export default async function NewToDo() {
     // Redirect or handle the case where the user is not authenticated
     return <div>You need to be authenticated to view this page.</div>;
   }
-  const testClick = () => {
-    "use client";
-    console.log("Test");
-  };
+
   return (
     <>
       <form action={newToDo}>
         <input name="title" className="bg-green-800" />
       </form>
       {todosData?.map((todo, i) => (
-        <p key={todo.id} onClick={testClick}>
-          {todo.title}
-        </p>
+        <p key={todo.id}>{todo.title}</p>
       ))}
     </>
   );
