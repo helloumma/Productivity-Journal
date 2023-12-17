344e8c6b9abcab3f74478bcd82627bdfdbde81f1
ummagohil <umgo0510@gmail.com>
2023-12-17 18:07:31 +0000

    remove unused api file

--- app/notes/api/revalidate.ts
+++
@@ -1,9 +0,0 @@
-export default async function revalidate(req: any, res: any) {
-  try {
-    await res.unstable_revalidate("/notes");
-    res.json({ revalidated: true });
-  } catch (error) {
-    console.error("Error during revalidation:", error);
-    res.status(500).json({ error: "Revalidation failed" });
-  }
-}
