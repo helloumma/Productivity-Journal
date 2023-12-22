9f683104b4477c8f2b08f7efe6617796c457fd61
ummagohil <umgo0510@gmail.com>
2023-12-22 13:23:47 +0000

    reminders delete functionality

--- components/Reminders.tsx
+++ components/Reminders.tsx
@@ -2,7 +2,7 @@
 
 // to do: add an onClick to each item - open up a modal and show the data that was mapped over
 
-import { getReminder, newReminder } from "@/actions/supabase";
+import { deleteReminder, getReminder, newReminder } from "@/actions/supabase";
 import { ReactNode, useEffect, useState } from "react";
 import { reminders } from "@/lib/reminders/types";
 import Modal from "./Modal";
@@ -36,6 +36,21 @@ export default function Reminders() {
   };
 
   const toggleModal = () => setShowModal(false);
+
+  const handleDelete = async (id: string) => {
+    // to do: refresh router path thing
+    await deleteReminder(id);
+
+    // if (success) {
+    //   // Remove the item from the state
+    //   setData(data.filter((item: ToDo) => item.id !== id));
+    // } else {
+    //   // Handle the error case
+    //   console.error("Failed to delete the item.");
+    // }
+
+    window.location.reload();
+  };
 
   return (
     <>
@@ -138,17 +153,19 @@ export default function Reminders() {
               </div>
             </div>
             <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-trash"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-              </svg>
+              <button onClick={() => handleDelete(reminders.id as any)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-trash"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                </svg>
+              </button>
             </div>
           </div>
         ))}
