c6a891573b8f084a3d241504541563f7c5bb6fae
ummagohil <umgo0510@gmail.com>
2023-12-27 19:58:18 +0000

    few changes to overall page layout

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -15,24 +15,32 @@ import NotesHeader from "@/components/NotesHeader";
 
 export default function Page() {
   return (
-    <div className=" p-4">
+    <div className="p-4">
       <div className="flex flex-wrap">
-        <div className="w-full p-4  border-slate-600 border">
+        <div className="w-full p-4 mx-8 my-2 rounded border-slate-600 border">
           <NotesHeader />
         </div>
-        <div className="w-1/2 border-slate-600 border">
-          <Schedule />
+        <div className="flex  mx-8 my-2 w-full">
+          <div className="w-full rounded border-slate-600 border flex">
+            <div className="w-1/2 border-r">
+              <Schedule />
+            </div>
+            <div className="w-1/2">
+              <NewToDo />
+            </div>
+          </div>
         </div>
-        <div className="w-1/2 border-slate-600 border">
-          <NewToDo />
+
+        <div className="flex w-full mx-8 my-2">
+          <div className="w-full mr-2 rounded border-slate-600 border">
+            <HabitTracker />
+          </div>
+          <div className="w-full ml-2 rounded border-slate-600 border">
+            <Reminders />
+          </div>
         </div>
-        <div className="w-1/2 border-slate-600 border">
-          <HabitTracker />
-        </div>
-        <div className="w-1/2 border-slate-600 border">
-          <Reminders />
-        </div>
-        <div className="w-full p-4  border-slate-600 border">
+
+        <div className="w-full p-4  mx-8 my-2 rounded border-slate-600 border">
           <MoodTracker />
         </div>
       </div>
