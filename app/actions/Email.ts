0b714174a413681b9001dc091728d3d4a083ea5a
ummagohil <umgo0510@gmail.com>
2024-04-28 14:02:48 +0100

    READ ME

---
+++ app/actions/Email.ts
@@ -0,0 +1,21 @@
+import { GithubAccessTokenEmail } from "../../components/EmailExample";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+import { getUser } from "./supabase";
+
+// send the email to the user's email
+// find a way to schedule the email given the time and date from the reminder set
+// within the 'GithubAccessTokenEmail' send the data from the reminder
+// the above needs to be done for EACH reminder set but how???
+
+export default async function SendEmail() {
+  const getUserInfo = await getUser();
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  const { data } = await resend.emails.send({
+    from: "hello@umma.dev",
+    to: "ummagohil@gmail.com",
+    subject: "Resend Test",
+    react: GithubAccessTokenEmail({ username: "zenorocha" }),
+  });
+  return data;
+}
---
+++ app/actions/supabase.ts
@@ -0,0 +1,157 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { defer } from "@defer/client";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import SendEmail from "../../defer/Email";
+import router from "next/navigation";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+const cookieStore = cookies();
+const supabase = createClient(cookieStore);
+const user = getUser();
+
+export async function getUser() {
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user;
+}
+
+export async function getToDo() {
+  const { data: todos } = await supabase
+    .from("todo")
+    .select()
+    .eq("userId", (await user)?.id);
+  return todos;
+}
+
+export async function newToDo(formData: FormData, state: any) {
+  const title = formData.get("titleToDo");
+  const time = formData.get("time");
+
+  await supabase.from("todo").insert({ title, userId: (await user)?.id, time });
+
+  revalidatePath("/notes");
+  return { message: true };
+}
+
+export async function deleteToDo(id: string) {
+  await supabase.from("todo").delete().eq("id", id);
+  revalidatePath("/notes");
+}
+
+export async function editToDo(title: any, id: string, time: any) {
+  const { data, error } = await supabase
+    .from("todo")
+    .update({ title: title, time: time })
+    .eq("id", id)
+    .select();
+  if (error) console.log(error);
+  revalidatePath("/notes");
+  return data;
+}
+
+export async function getReminder() {
+  const { data: reminder } = await supabase
+    .from("reminders")
+    .select()
+    .eq("userId", (await user)?.id);
+  return reminder;
+}
+
+export async function newReminder(
+  formData: FormData,
+  prevState: {
+    message: string;
+  }
+) {
+  const reminder = formData.get("reminder");
+  const time = formData.get("time");
+  const date = formData.get("date");
+  try {
+    await supabase
+      .from("reminders")
+      .insert({ reminder, time, date, userId: (await user)?.id });
+    revalidatePath("/notes");
+    return { message: true };
+  } catch (err) {
+    return { message: err };
+  }
+
+  // if (!response.error) {
+  //   const sendTime: any = new Date(`${date} ${time}`);
+
+  //   SendEmail();
+  // }
+  // const sendTime = new Date(`${date} ${time}`);
+  // defer(SendEmail, sendTime as any);
+
+  // console.log(sendTime);
+  // console.log(new Date());
+  // const currTime = new Date();
+  // currTime.setSeconds(0);
+  // console.log(sendTime === currTime);
+}
+
+export async function deleteReminder(id: string) {
+  await supabase.from("reminders").delete().eq("id", id);
+  revalidatePath("/notes");
+}
+
+export async function getHabit() {
+  const { data: habit } = await supabase
+    .from("habitTracker")
+    .select()
+    .eq("userId", (await user)?.id);
+  return habit;
+}
+
+export async function newHabit(formData: FormData) {
+  const habit = formData.get("habit");
+  const emoji = formData.get("emoji");
+  await supabase
+    .from("habitTracker")
+    .insert({ habit, emoji, userId: (await user)?.id });
+
+  revalidatePath("/notes");
+  console.log(formData);
+}
+
+export async function deleteHabit(id: string) {
+  await supabase.from("habitTracker").delete().eq("id", id);
+  revalidatePath("/notes");
+}
+
+export async function editHabit(habit: any, id: string, emoji: any) {
+  const { data, error } = await supabase
+    .from("habitTracker")
+    .update({ habit: habit, emoji: emoji })
+    .eq("id", id)
+    .select();
+  if (error) console.log(error);
+  revalidatePath("/notes");
+  console.log(data);
+  return data;
+}
+
+export async function getSchedule() {
+  const { data: schedule } = await supabase
+    .from("schedule")
+    .select()
+    .eq("userId", (await user)?.id);
+  return schedule;
+}
+
+// only use for testing purposes
+export async function newSchedule(formData: FormData) {
+  const item = formData.get("item");
+  await supabase
+    .from("schedule")
+    .insert({ item, userId: (await user)?.id })
+    .select();
+  revalidatePath("/notes");
+}
